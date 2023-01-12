c93c4d335624c969eeb8ca3ca522163f9512d91c
Saha Sani <s2300109@BC-C02GR0BUQ6LR-MacBook-Air.local>
2023-01-12 15:21:14 +0200

    testing git

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -6,7 +6,7 @@ const Blog = () => {
         <div>
             <Header></Header>
             <div className='min-h-screen bg-purple-300 pt-20 lg:pt-80'>
-                <h1 className='text-center font-bold text-xl lg:text-5xl'>Coming soon......</h1>
+                <h1 className='text-center font-bold text-xl lg:text-5xl'>Coming soon.......</h1>
             </div>
         </div>
     );
